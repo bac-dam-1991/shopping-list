a47e0ffad4cbace261f2a55c1e6c911e6fb34ed4
Bac Trach Dam <bac.dam.1991@gmail.com>
2022-10-18 22:40:54 +1100

    Video: Getting all shopping lists

--- back-end/index.ts
+++ back-end/index.ts
@@ -2,6 +2,21 @@ import express from 'express';
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const MockShoppingLists = [
+	{
+		id: '1',
+		name: 'Grocery',
+	},
+	{
+		id: '2',
+		name: 'Pharmacy',
+	},
+];
+
+app.get('/api/v1/shopping-lists', (req, res) => {
+	res.status(200).json(MockShoppingLists);
+});
+
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}...`);
 });
