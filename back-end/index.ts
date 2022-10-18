7250422e616b64f00b89205ebef3fefe76f3122a
Bac Trach Dam <bac.dam.1991@gmail.com>
2022-10-18 22:55:57 +1100

    Video: Adding a new shopping list

--- back-end/index.ts
+++ back-end/index.ts
@@ -13,6 +13,8 @@ const MockShoppingLists = [
 	},
 ];
 
+app.use(express.json());
+
 app.get('/api/v1/shopping-lists', (req, res) => {
 	res.status(200).json(MockShoppingLists);
 });
@@ -23,6 +25,14 @@ app.get('/api/v1/shopping-lists/:id', (req, res) => {
 		return list.id === id;
 	});
 	res.status(200).json(shoppingList);
+});
+
+app.post('/api/v1/shopping-lists', (req, res) => {
+	const { name } = req.body;
+	const nextId = MockShoppingLists.length + 1;
+	const newList = { id: nextId.toString(), name };
+	MockShoppingLists.push(newList);
+	res.status(201).json(newList);
 });
 
 app.listen(PORT, () => {
