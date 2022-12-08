003df9c7c18880da714449b85ce0c491e057a2a9
Bac Trach Dam <bac.dam.1991@gmail.com>
2022-12-08 19:44:16 +1100

    Video: Building React App

--- front-end/src/apis/client.tsx
+++ front-end/src/apis/client.tsx
@@ -2,17 +2,13 @@ import { useAuth0 } from '@auth0/auth0-react';
 import axios from 'axios';
 import { useCallback } from 'react';
 
-export const Client = axios.create({
-	baseURL: 'http://localhost:3001/api/v1/',
-});
-
 export const useHttpClient = () => {
 	const { getAccessTokenSilently } = useAuth0();
 
 	const getClient = useCallback(async () => {
 		const token = await getAccessTokenSilently();
 		return axios.create({
-			baseURL: 'http://localhost:3001/api/v1/',
+			baseURL: process.env.REACT_APP_SERVER,
 			headers: {
 				Authorization: `Bearer ${token}`,
 			},
--- front-end/src/components/AuthProviderWithHistory.tsx
+++ front-end/src/components/AuthProviderWithHistory.tsx
@@ -6,12 +6,13 @@ export interface Auth0ProviderWithHistoryProps {
 	children: ReactNode;
 }
 
+const domain = process.env.REACT_APP_AUTH0_DOMAIN!;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID!;
+const audience = process.env.REACT_APP_AUTH0_AUDIENCE!;
+
 export const Auth0ProviderWithHistory = ({
 	children,
 }: Auth0ProviderWithHistoryProps) => {
-	const domain = 'shopping-list.au.auth0.com';
-	const clientId = '6b8VEO6sia65RcUtW6GQsptuxyfPv0y4';
-
 	const navigate = useNavigate();
 
 	const onRedirectCallback = (appState?: AppState) => {
@@ -24,7 +25,7 @@ export const Auth0ProviderWithHistory = ({
 			clientId={clientId}
 			redirectUri={window.location.origin}
 			onRedirectCallback={onRedirectCallback}
-			audience="http://shopping-list/app"
+			audience={audience}
 		>
 			{children}
 		</Auth0Provider>
