cb12bd70a501bf6e7da79ec64a341cdfb71650f9
Bac Dam <bac.dam.1991@gmail.com>
2022-12-26 16:11:52 +1100

    Added responsive drawer.

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,27 +1,28 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { ShoppingListView } from './views/ShoppingListView';
-import { HomeView } from './views/HomeView';
-import { NotFoundView } from './views/NotFoundView';
-import { AllShoppingListsView } from './views/AllShoppingListsView';
-import { Auth0ProviderWithHistory } from './components/AuthProviderWithHistory';
-import { Navbar } from './components/Navbar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ShoppingListView } from "./views/ShoppingListView";
+import { HomeView } from "./views/HomeView";
+import { NotFoundView } from "./views/NotFoundView";
+import { AllShoppingListsView } from "./views/AllShoppingListsView";
+import { Auth0ProviderWithHistory } from "./components/AuthProviderWithHistory";
+import { Navbar } from "./components/Navbar";
 
 const App = () => {
-	return (
-		<BrowserRouter>
-			<Auth0ProviderWithHistory>
-				<Navbar />
-				<Routes>
-					<Route path="/" element={<HomeView />} />
-					<Route path="/shopping-lists">
-						<Route index element={<AllShoppingListsView />} />
-						<Route path=":id" element={<ShoppingListView />} />
-					</Route>
-					<Route path="*" element={<NotFoundView />} />
-				</Routes>
-			</Auth0ProviderWithHistory>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Auth0ProviderWithHistory>
+        <Navbar>
+          <Routes>
+            <Route path="/" element={<HomeView />} />
+            <Route path="/shopping-lists">
+              <Route index element={<AllShoppingListsView />} />
+              <Route path=":id" element={<ShoppingListView />} />
+            </Route>
+            <Route path="*" element={<NotFoundView />} />
+          </Routes>
+        </Navbar>
+      </Auth0ProviderWithHistory>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- front-end/src/components/Navbar.tsx
+++ front-end/src/components/Navbar.tsx
@@ -1,25 +1,119 @@
-import { AppBar, Toolbar, Stack, Button } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
-import { AuthButton } from './AuthButton';
+import { ReactNode, useState } from "react";
+import {
+  List,
+  ListItem,
+  ListItemButton,
+  AppBar,
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  ListItemText,
+  Toolbar,
+  Typography,
+  Button,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Link } from "react-router-dom";
+import { AuthButton } from "./AuthButton";
 
-export const Navbar = () => {
-	const navigate = useNavigate();
+const DrawerWidth = 240;
+const navItems = [
+  { label: "Home", path: "/" },
+  { label: "Shopping Lists", path: "/shopping-lists" },
+];
+export interface NavbarProps {
+  children: ReactNode;
+}
 
-	return (
-		<AppBar position="static">
-			<Toolbar>
-				<Stack sx={{ flexGrow: 1, alignItems: 'start' }}>
-					<Button onClick={() => navigate('/')} color="inherit">
-						Home
-					</Button>
-				</Stack>
-				<Stack spacing={2} direction="row">
-					<Button color="inherit" onClick={() => navigate('/shopping-lists')}>
-						Shopping Lists
-					</Button>
-					<AuthButton />
-				</Stack>
-			</Toolbar>
-		</AppBar>
-	);
+export const Navbar = ({ children }: NavbarProps) => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        Shopping List App
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item.path} disablePadding>
+            <ListItemButton
+              component={Link}
+              to={item.path}
+              sx={{ textAlign: "center" }}
+            >
+              <ListItemText primary={item.label} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <AuthButton />
+    </Box>
+  );
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <AppBar component="nav">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            Shopping List App
+          </Typography>
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            {navItems.map((item) => (
+              <Button
+                component={Link}
+                to={item.path}
+                key={item.path}
+                sx={{ color: "#fff" }}
+              >
+                {item.label}
+              </Button>
+            ))}
+            <AuthButton />
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true,
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: DrawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box component="main" sx={{ width: "100%" }}>
+        <Toolbar />
+        {children}
+      </Box>
+    </Box>
+  );
 };
